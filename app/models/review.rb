311126bdd1ea615f2b1860aa0f8b2689db59b1ea
adrienne robert vassy <adrienne.robertvassy@gmail.com>
2022-08-17 12:46:55 +0200

    add phone num

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, in: (0..5) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_101838) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_104610) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
   create_table "reviews", force: :cascade do |t|
