5de94786413ee9cf84c838b6168894955d26deab
adrienne robert vassy <adrienne.robertvassy@gmail.com>
2022-08-17 14:17:42 +0200

    finished models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
-  validates :content, :rating, presence: true
-  validates :rating, numericality: { only_integer: true, in: (0..5) }
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true, in: 0..5 }
 end
